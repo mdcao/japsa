458e0b8abb0c7c7381d00fdc93f5b1968dcf334a
Son Hoang Nguyen <s.hoangnguyen@imb.uq.edu.au>
2016-12-19 11:39:22 +1000

    npScarf v1.6-12 about to release

--- src/main/java/japsa/tools/bio/np/GapCloserCmd.java
+++ src/main/java/japsa/tools/bio/np/GapCloserCmd.java
@@ -38,9 +38,12 @@ import japsa.bio.hts.scaffold.ContigBridge;
 import japsa.bio.hts.scaffold.RealtimeScaffolding;
 import japsa.bio.hts.scaffold.ScaffoldGraph;
 import japsa.bio.hts.scaffold.ScaffoldGraphDFS;
+import japsa.seq.SequenceReader;
 import japsa.util.CommandLine;
 import japsa.util.Logging;
 import japsa.util.deploy.Deployable;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 
@@ -50,9 +53,9 @@ import java.io.IOException;
  */
 @Deployable(
 		scriptName = "jsa.np.npscarf",
-		scriptDesc = "Scaffold and finish assemblies using Oxford Nanopore sequencing reads",
+		scriptDesc = "Experimental Scaffold and finish assemblies using Oxford Nanopore sequencing reads",
 		seeAlso = "jsa.np.npreader, jsa.util.streamServer, jsa.util.streamClient"
-)
+		)
 public class GapCloserCmd extends CommandLine{
 
 	public GapCloserCmd(){
@@ -62,8 +65,15 @@ public class GapCloserCmd extends CommandLine{
 		setDesc(annotation.scriptDesc());
 
 		addString("seqFile", null, "Name of the assembly file (sorted by length)",true);
-		addString("bamFile", null, "Name of the bam file", true);
-		addString("spadesDir", null, "Name of the output folder by SPAdes. Experimental.");
+		//addString("bamFile", null, "Name of the bam file", true);
+
+		addString("input", "-", "Name of the input file, - for stdin", true);
+		addString("format", "fastq/fasta", "format of the input fastq/fasta or sam/bam");
+		addString("bwaExe", "bwa", "Path to bwa");
+		addInt("bwaThread", 4, "Theads used by bwa");
+		addBoolean("long", false, "Whether report all sequences, including short/repeat contigs (default) or only long/unique/completed sequences.");
+		
+		addString("spadesDir", null, "Name of the output folder by SPAdes: assembly graph and paths will be used for better gap-filling.");
 		addString("prefix", "out", "Prefix for the output files");	
 		addString("genes", null , "Realtime annotation: name of annotated genes in GFF 3.0 format");
 		addString("resistGene", null , "Realtime annotation: name of antibiotic resistance gene fasta file");
@@ -72,18 +82,18 @@ public class GapCloserCmd extends CommandLine{
 		//addInt("marginThres", 1000, "Margin threshold: to limit distance to the contig's ends of the alignment used in bridging."); 
 		addInt("minContig", 300, "Minimum contigs length that are used in scaffolding."); 
 		addInt("maxRepeat", 7500, "Maximum length of repeat in considering species."); 
-		
+
 		addDouble("cov", 0, "Expected average coverage of Illumina, <=0 to estimate");
 		addInt("qual", 1, "Minimum quality");
 		addInt("support", 1, "Minimum supporting long read needed for a link between markers");
-	
+
 		addBoolean("realtime", false, "Process in real-time mode. Default is batch mode (false)");
-		addInt("read", 500,  "Minimum number of reads between analyses");		
-		addInt("time", 300,   "Minimum number of seconds between analyses");
+		addInt("read", 50,  "Minimum number of reads between analyses");		
+		addInt("time", 10,   "Minimum number of seconds between analyses");
 		addBoolean("verbose", false, "Turn on debugging mode");
 
 		addStdHelp();		
-		
+
 	} 	
 	//static boolean hardClip = false;
 
@@ -94,19 +104,64 @@ public class GapCloserCmd extends CommandLine{
 
 		/***********************************************************************/
 		String prefix = cmdLine.getStringVal("prefix");
-		String bamFile = cmdLine.getStringVal("bamFile");
+		//String bamFile = cmdLine.getStringVal("bamFile");
+
+		String input = cmdLine.getStringVal("input");
+		String bwaExe = cmdLine.getStringVal("bwaExe");
+		int bwaThread = cmdLine.getIntVal("bwaThread");
+		String format = cmdLine.getStringVal("format").toLowerCase();
 
 		String sequenceFile = cmdLine.getStringVal("seqFile"),
 				spadesFolder = cmdLine.getStringVal("spadesDir"),
-				
+
 				genesFile = cmdLine.getStringVal("genes"),
 				resistFile = cmdLine.getStringVal("resistGene"),
 				isFile = cmdLine.getStringVal("insertSeq"),
 				oriFile = cmdLine.getStringVal("oriRep");
-		
-		
+
+
 		File 	graphFile = new File(spadesFolder+"/assembly_graph.fastg"),
 				pathFile = new File(spadesFolder+"/contigs.paths");
+
+
+		//TODO: need to validate bwa if fastq is the input		
+		if (format.startsWith("fastq") ||
+				format.startsWith("fasta") ||
+				format.startsWith("fq") ||
+				format.startsWith("fa")){
+			try{
+				ProcessBuilder pb = new ProcessBuilder(bwaExe).redirectErrorStream(true);
+				Process process =  pb.start();
+				BufferedReader bf = SequenceReader.openFile(process.getInputStream());
+				String line;
+				String version = "";
+				while ((line = bf.readLine())!=null){
+					if (line.startsWith("Version: ")){
+						version = line.substring(9).trim();
+						break;//while
+					}				
+				}	
+				bf.close();
+				if (version.length() == 0){
+					System.err.println(bwaExe + " is not the rith path to bwa. bwa is required");
+					System.exit(1);
+				}else{
+					if (!version.startsWith("0.7.1")){
+						System.err.println(" Require bwa of 0.7.11 or above");
+						System.exit(1);
+					}
+				}
+			}catch (IOException e){
+				System.err.println(e.getMessage());
+				System.exit(1);
+			}
+
+		}else if (format.startsWith("sam") || format.startsWith("bam")){
+			// no problem
+		}else{
+			System.err.println("I dont understand format " + format);
+			System.exit(1);
+		}
 
 		if(spadesFolder !=null && graphFile.exists() && pathFile.exists())
 			Logging.info("===> Use assembly graph and path from SPAdes!");
@@ -115,11 +170,23 @@ public class GapCloserCmd extends CommandLine{
 			spadesFolder=null;
 		}
 
-		
+		//ProcessBuilder pb = new ProcessBuilder(bwaExe,  
+		//		"mem",
+		//		
+		//		"-k", "21,33,55,77,99,127", 
+		//		"--careful",
+		//		"--pe1-1", String.valueOf(sampleRecord.readFile1),
+		//		"--pe1-2", String.valueOf(sampleRecord.readFile2),
+		//		"-m", "60",
+		//		"-t", String.valueOf(threads),
+		//		"-o", assemblyPath + "spades_output_" + sampleID
+		//		);
+
+
 		int 	//marginThres = cmdLine.getIntVal("marginThres"),
-				minContig = cmdLine.getIntVal("minContig"),
-				minSupport = cmdLine.getIntVal("support"),
-				maxRepeat = cmdLine.getIntVal("maxRepeat");
+		minContig = cmdLine.getIntVal("minContig"),
+		minSupport = cmdLine.getIntVal("support"),
+		maxRepeat = cmdLine.getIntVal("maxRepeat");
 		//if(marginThres < 0)
 		//	Logging.exit("Marginal threshold must not be negative", 1);			
 		if(minContig <= 0)
@@ -128,23 +195,23 @@ public class GapCloserCmd extends CommandLine{
 			Logging.exit("Minimum supporting reads has to be positive", 1);
 		if(maxRepeat <= 0)
 			Logging.exit("Maximal possible repeat length has to be positive", 1);
-			
-		
+
+
 		ScaffoldGraph.minContigLength = minContig;
 		ScaffoldGraph.minSupportReads = minSupport;	
 		ScaffoldGraph.maxRepeatLength = maxRepeat;		
 		//ScaffoldGraph.marginThres = marginThres;
 		ScaffoldGraph.verbose = cmdLine.getBooleanVal("verbose");
-			
-		
+		ScaffoldGraph.reportAll = !cmdLine.getBooleanVal("long");
+
 		double cov = cmdLine.getDoubleVal("cov");
 		int qual = cmdLine.getIntVal("qual");
 		if(qual < 0)
 			Logging.exit("Phred score of quality has to be positive", 1);
-		
+
 		int number = cmdLine.getIntVal("read"),
-			time = cmdLine.getIntVal("time");
-		
+				time = cmdLine.getIntVal("time");
+
 		if(number <= 0)
 			Logging.exit("Number of reads has to be positive", 1);			
 		if(time < 0)
@@ -166,9 +233,9 @@ public class GapCloserCmd extends CommandLine{
 				}
 			if (cov <=0)
 				cov = ScaffoldGraph.estimatedCov;
-			
-			rtScaffolding.scaffolding(bamFile, number, time, cov/1.6, qual);
-			
+
+			rtScaffolding.scaffolding2(input, number, time, cov/1.6, qual, format, bwaExe, bwaThread, sequenceFile);
+
 		}
 		else{
 			graph = new ScaffoldGraphDFS(sequenceFile, genesFile, resistFile, isFile, oriFile);
@@ -177,13 +244,13 @@ public class GapCloserCmd extends CommandLine{
 
 			if (cov <=0)
 				cov = ScaffoldGraph.estimatedCov;
-			
-			graph.makeConnections(bamFile, cov / 1.6, qual);
-	
+
+			graph.makeConnections2(input, cov / 1.6, qual, format, bwaExe, bwaThread, sequenceFile);
+
 			graph.connectBridges();
 			if(prefix != null)
 				graph.prefix = prefix;
-	
+
 			ContigBridge.forceFilling();
 			graph.printSequences();
 		}
@@ -193,10 +260,10 @@ public class GapCloserCmd extends CommandLine{
 
 /*RST*
 ---------------------------------------------------------------------------------------
-*npScaffolder*: real-time scaffolder using SPAdes contigs and Nanopore sequencing reads
+ *npScaffolder*: real-time scaffolder using SPAdes contigs and Nanopore sequencing reads
 ---------------------------------------------------------------------------------------
 
-*npScaffolder* (jsa.np.npscarf) is a program that connect contigs from a draft genomes 
+ *npScaffolder* (jsa.np.npscarf) is a program that connect contigs from a draft genomes 
 to generate sequences that are closer to finish. These pipelines can run on a single laptop
 for microbial datasets. In real-time mode, it can be integrated with simple structural 
 analyses such as gene ordering, plasmid forming.
@@ -212,30 +279,38 @@ Usage examples
 A summary of *npScarf* usage can be obtained by invoking the --help option::
 
     jsa.np.npscarf --help
-    
+
 Input
 =====
-*npScarf* takes two files as required input::
+ *npScarf* takes two files as required input::
 
-	jsa.np.npscarf -s <*draft*> -b <*bam*>
-	
+	jsa.np.npscarf -seq <*draft*> -input <*nanopore*>
+
 <*draft*> input is the FASTA file containing the pre-assemblies. Normally this 
 is the output from running SPAdes on Illumina MiSeq paired end reads.
 
-<*bam*> contains SAM/BAM formated alignments between <*draft*> file and <*nanopore*> 
-FASTA/FASTQ file of long read data. We use BWA-MEM as the recommended aligner 
+<*nanopore*> is either the long reads in FASTA/FASTQ file or SAM/BAM formated alignments 
+between them to <*draft*> file. We use BWA-MEM as the recommended aligner 
 with the fixed parameter set as follow::
 
 	bwa mem -k11 -W20 -r10 -A1 -B1 -O1 -E1 -L0 -a -Y <*draft*> <*nanopore*> > <*bam*>
 	
+The input file format is specified by option --format. The default is FASTA/FASTQ in which 
+the path to BWA version 0.7.11 or newer is required. Remember to always *INDEXING* the 
+reference before running BWA::
+	
+	bwa index <*draft*>
+	
+	Missing this step would break down the whole pipeline.
+
 Output
 =======
-*npScarf* output is specified by *-prefix* option. The default prefix is \'out\'.
+ *npScarf* output is specified by *-prefix* option. The default prefix is \'out\'.
 Normally the tool generate two files: *prefix*.fin.fasta and *prefix*.fin.japsa which 
 indicate the result scaffolders in FASTA and JAPSA format.
 
 In realtime mode, if any annotation analysis is enabled, a file named 
-*prefix*.anno.japsa is generated instead. This file contains features detected after
+ *prefix*.anno.japsa is generated instead. This file contains features detected after
 scaffolding.
 
 Real-time scaffolding
@@ -252,15 +327,20 @@ npReader is the module that provides such data from fast5 files returned from th
 base-calling cloud service Metrichor. Ones can run::
 
     jsa.np.npreader -realtime -folder c:\Downloads\ -fail -output - | \
+      jsa.np.npscarf --realtime -bwaExe=<path_to_BWA> -bwaThread=10 -input - -seq <*draft*> > log.out 2>&1
+    
+For the same purpose, you can also invoke BWA-MEM explicitly as in the old version of *npScarf*,
+In this case, option --format=SAM must be presented as follow:
+      
+    jsa.np.npreader -realtime -folder c:\Downloads\ -fail -output - | \
       bwa mem -t 10 -k11 -W20 -r10 -A1 -B1 -O1 -E1 -L0 -a -Y -K 3000 <*draft*> - 2> /dev/null | \ 
-      jsa.np.npscarf --realtime -b - -seq <*draft*> > log.out 2>&1
+      jsa.np.npscarf --realtime -input - -format=SAM -seq <*draft*> > log.out 2>&1
 
 or if you have the whole set of Nanopore long reads already and want to emulate the 
 streaming mode::
 
     jsa.np.timeEmulate -s 100 -i <*nanopore*> -output - | \
-      bwa mem -t 10 -k11 -W20 -r10 -A1 -B1 -O1 -E1 -L0 -a -Y -K 3000 <*draft*> - 2> /dev/null | \ 
-      jsa.np.npscarf --realtime -b - -seq <*draft*> > log.out 2>&1
+      jsa.np.npscarf --realtime -bwaExe=<path_to_BWA> -bwaThread=10 -input - -seq <*draft*> > log.out 2>&1
 
 Note that jsa.np.timeEmulate based on the field *timestamp* located in the read name line to
 decide the order of streaming data. So if your input <*nanopore*> already contains the field,
@@ -280,18 +360,16 @@ The tool includes usecase for streaming annotation. Ones can provides database o
 resistance genes and/or Origin of Replication in FASTA format for the analysis of gene ordering
 and/or plasmid identifying respectively::
 
-    jsa.np.timeEmulate -s 100 -i <*nanopore*> -output - | \
-      bwa mem -t 10 -k11 -W20 -r10 -A1 -B1 -O1 -E1 -L0 -a -Y -K 3000 <*draft*> - 2> /dev/null | \ 
-      jsa.np.npscarf --realtime -b - -seq <*draft*> -resistGene <*resistDB*> -oriRep <*origDB*> > log.out 2>&1
+    jsa.np.timeEmulate -s 100 -i <*nanopore*> -output - | \ 
+      jsa.np.npscarf --realtime -bwaExe=<path_to_bwa> -input - -seq <*draft*> -resistGene <*resistDB*> -oriRep <*origDB*> > log.out 2>&1
 
 Assembly graph
 ==============
-*npScarf* can read the assembly graph info from SPAdes to make the results more precise down to SNP level.
-This function is still on development and the results might be slightly deviate from the stable version in
-term of number of final contigs::
+ *npScarf* can read the assembly graph info from SPAdes to make the results more precise.
+The results might be slightly deviate from the old version in term of number of final contigs::
 
     jsa.np.npscarf --spadesFolder=<SPAdes_output_directory> <options...>
 
 where SPAdes_output_directory indicates the result folder of SPAdes, containing files such as contigs.fasta, 
 contigs.paths and assembly_graph.fastg.
-*RST*/
+ *RST*/
