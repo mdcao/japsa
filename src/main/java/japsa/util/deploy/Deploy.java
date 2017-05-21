dabe101a7ba224039f9ac81f19e52f17e58f892e
Minh Duc Cao <minhduc.cao@gmail.com>
2017-05-21 20:55:33 +1000

    reduce guava dependency

--- src/main/java/japsa/util/deploy/Deploy.java
+++ src/main/java/japsa/util/deploy/Deploy.java
@@ -102,7 +102,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
