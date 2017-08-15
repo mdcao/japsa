fd643e0fa10e5cf2a43d0064d85faa5441fe46ba
Son Nguyen <s.hoangnguyen@imb.uq.edu.au>
2017-08-15 15:34:50 +1000

    reverting RealtimeSpeciesTyping.java: where it worked!

--- src/main/java/japsa/bio/np/RealtimeSpeciesTyping.java
+++ src/main/java/japsa/bio/np/RealtimeSpeciesTyping.java
@@ -56,6 +56,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
+import org.rosuda.JRI.REXP;
+import org.rosuda.JRI.Rengine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -367,16 +369,27 @@ public class RealtimeSpeciesTyping {
 //	}	
 	
 	public static class RealtimeSpeciesTyper extends RealtimeAnalysis{
-		MultinomialCI rengine;
+		Rengine rengine;
 		RealtimeSpeciesTyping typing;
 		public SequenceOutputStream countsOS;
 
 		public RealtimeSpeciesTyper(RealtimeSpeciesTyping t, String output) throws IOException{
 			typing = t;
 			//Set up Rengine
-			rengine = new MultinomialCI(0.05);
+			if (!Rengine.versionCheck()) {
+				LOG.error("** JRI R-Engine: Version mismatch - Java files don't match library version.");
+				System.exit(1);
+			}
+			//Rengine.DEBUG=1;
+			rengine = new Rengine (new String [] {"--no-save"}, false, null);
+			if (!rengine.waitForR()){
+				LOG.error("Cannot load R");
+				System.exit(1);
+			}    
+			rengine.eval("library(MultinomialCI)");
+			rengine.eval("alpha<-0.05");
 
-			
+			LOG.info("REngine ready");
 			countsOS = SequenceOutputStream.makeOutputStream(output);
 			if(!JSON)
 				countsOS.print("time\tstep\treads\tbases\tspecies\tprob\terr\ttAligned\tsAligned\n");
@@ -412,10 +425,10 @@ public class RealtimeSpeciesTyping {
 			countArray.add(1);
 			speciesArray.add("others");		
 
-			rengine.assignCount(countArray.toArray());
-			rengine.eval();        
-			//REXP tab  = rengine.eval("tab",true);  
-			double [][] results =rengine.tab();
+			rengine.assign("count", countArray.toArray());
+			rengine.eval("tab = multinomialCI(count,alpha)");        
+			REXP tab  = rengine.eval("tab",true);  
+			double [][] results = tab.asDoubleMatrix();
 
 
 			if(JSON)
@@ -457,7 +470,7 @@ public class RealtimeSpeciesTyping {
 
 		protected void close(){
 			try{
-				//rengine.end();
+				rengine.end();
 				countsOS.close();
 			}catch (Exception e){
 				e.printStackTrace();
