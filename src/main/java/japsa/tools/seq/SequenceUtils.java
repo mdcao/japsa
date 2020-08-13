7a41895bc7cb7f98e2fe0925638173336f1c6aeb
Lachlan Coin <ljmcoinwork@gmail.com>
2020-08-13 10:09:40 +1000

    simplified CombinedIterator

--- src/main/java/japsa/tools/seq/SequenceUtils.java
+++ src/main/java/japsa/tools/seq/SequenceUtils.java
@@ -34,8 +34,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import htsjdk.samtools.SAMRecord;
 import htsjdk.samtools.SAMRecordIterator;
@@ -129,154 +133,73 @@ public class SequenceUtils {
 	public static String src_tag = "SC";
 	public static String pool_tag = "PT";
 	
-	public static  class CombinedIterator implements Iterator<SAMRecord> {
-		private final Iterator<SAMRecord>[] samIters;
-		int current_sam_index =0;
-		int currentIndex =0; //relates to chromosome
-		private final SAMRecord[] currentVals;
-		private final boolean[] returned;
-		private final int[] cnts ;
-		int max;
-		Collection<String>[] readList ;
-		Map<Integer, int[]> chrs;
-		public  CombinedIterator(Iterator<SAMRecord>[] samIters, int max, Collection<String>[]readList, Map<Integer, int[]> chrs) {
-			this.samIters = samIters;
-			this.readList = readList;
-			currentVals = new SAMRecord[samIters.length];
-			returned = new boolean [samIters.length];
-			this.max = max;
-			cnts = new int[samIters.length];
-			this.chrs = chrs.size()>0 ? chrs : null;
-		}
-		/*public void close(){
-			for(int i=0; i<samIters.length; i++){
-				samIters[i].close();
-			}
-		}*/
-		@Override
-		public boolean hasNext() {
-			for(int i=0; i<samIters.length; i++){
-				if(samIters[i].hasNext() && cnts[i]<max) return true;
-			}
-		//	this.close();
-			return false;
-		}
-		int pool_ind=-1;
-		private SAMRecord next(int i){
-			SAMRecord sr;
-			if(currentVals[i]!=null && !returned[i]){
-				sr = currentVals[i];
-			}else{
-				if(cnts[i]<max){
-					
-					//sr  = samIters[i].next();
-				/*	while(!(
-							sr==null || 
-							(readList==null || readList.contains(sr.getReadName())) ||
-							(chrs==null || chrs.contains(sr.getReferenceIndex()))
-							)
-							)*/
-					inner: while(true)
-					{
-						sr  = samIters[i].next();
-						if(sr==null) break inner;
-						pool_ind =-1;
-						if(readList!=null){
-							inner1: for(int i2=0; i2<readList.length; i2++){
-								if(readList[i2].contains(sr.getReadName())){
-									pool_ind = i2;
-									break inner1;
-								}
-							}
-						}
-						if(readList!=null && pool_ind>=0) break inner;
-						if(readList==null &&  chrs!=null && contains(chrs,sr)) break inner;
-						if(readList==null  && chrs==null) break inner;
-					}
-					if(sr!=null) {
-						sr.setAttribute(pool_tag, pool_ind);
-						sr.setAttribute(src_tag, i);
-					}
-					currentVals[i] = sr;
-					returned[i] = false; 
-				}else{
-					sr = null;
-				}
-			}
-			return sr;
+	 static class SamR implements Comparable{
+		final int pos, ref, ind;
+		public SamR(SAMRecord samRecord, int i) {
+			ref = samRecord.getReferenceIndex();
+			pos = samRecord.getAlignmentStart();
+			ind = i;
+			samRecord.setAttribute(SequenceUtils.src_tag,i);
 		}
 
-		private boolean contains(Map<Integer, int[]> chrs2, SAMRecord sr) {
-			int[] obj = chrs2.get(sr.getReferenceIndex());
-			if(obj==null) return false;
-			else{
-				if(sr.getAlignmentStart()>=obj[0] && sr.getAlignmentEnd() <=obj[1]) return true;
-				else return false;
-			}
-		}
 		@Override
-		public SAMRecord next() {
-			//int curr_index = current_sam_index;
-			SAMRecord sr = next(current_sam_index);
-			
-			if(sr==null || sr.getReferenceIndex()>currentIndex ){
-				int[] ref_inds = new int[samIters.length];
-				int min_ind =-1;
-				int minv = Integer.MAX_VALUE;
-				for(int i=0; i<samIters.length; i++){
-					SAMRecord sr_i = next(i);
-					if(sr_i!=null) {
-						ref_inds[i] = sr_i.getReferenceIndex(); //note this only advances if not returned;
-						if(ref_inds[i]<minv){
-							min_ind = i;
-							minv = ref_inds[i];
-						}
-					}
-				}
-				current_sam_index = min_ind;
-			}
-			
-			if(current_sam_index<0) return null;
-			sr = this.currentVals[current_sam_index];
-			if(sr!=null) this.currentIndex = sr.getReferenceIndex();
-			cnts[current_sam_index]++;
-			returned[current_sam_index] = true; 
-			return sr;
-			
+		public int compareTo(Object o) {
+			SamR o1 = (SamR)o;
+			int res = Integer.compare(ref, o1.ref);
+			if(res==0) res = Integer.compare(pos, o1.pos);
+			return res;
 		}
-	}
-	public static Iterator<SAMRecord>  getCombined(Iterator<SAMRecord>[] samReaders, Collection[] reads, int max_reads,String chroms){
-		Map<Integer, int[]>chromIndices = new HashMap<Integer, int[]>();
-		return getCombined(samReaders,reads, max_reads, chroms, chromIndices);
+		
 	}
 	
-	//chroms is string e.g 0:1:2  or 0,0,2400000:1,0,240000
-public static Iterator<SAMRecord>  getCombined(Iterator<SAMRecord>[] samReaders, Collection[] reads, int max_reads,String chroms,
-		Map<Integer, int[]>chromIndices ){
-//	Map<Integer, int[] > chromIndices = null;
-	if(chroms!=null && !chroms.equals("all")){
+	public static class CombinedIterator implements Iterator<SAMRecord> {
+		private final Iterator<SAMRecord>[] samIters;
+		private final SAMRecord[] currentVals; 
+		private final SortedSet<SamR> samR = new TreeSet<SamR>();
 		
-		String[] chri = chroms.split(":");
-		for(int i=0; i<chri.length; i++){
-			String[] str = chri[i].split(",");
-			int[] vals ;
-			if(str.length==1) vals = new int[] {0,Integer.MAX_VALUE};
-			else{
-				vals = new int[] {Integer.parseInt(str[1]), Integer.parseInt(str[2])};
+		final int len;
+		public  CombinedIterator(Iterator<SAMRecord>[] samIters) {
+			this.samIters = samIters;
+			len = samIters.length; 
+			currentVals = new SAMRecord[samIters.length];
+			for(int i=0; i<len; i++){
+				if(samIters[i].hasNext()){
+					currentVals[i] =   samIters[i].next();
+					samR.add(new SamR(currentVals[i],i));
+				}
 			}
-			chromIndices.put(Integer.parseInt(str[0]), vals);
 		}
+		
+		@Override
+		public boolean hasNext() {
+			return samR.size()>0;
+		}
+
+		@Override
+		public SAMRecord next() {
+			SamR  first = samR.first(); //next element
+			samR.remove(first);
+			SAMRecord sr = currentVals[first.ind];
+			if(samIters[first.ind].hasNext()){
+				//replace element removed if possible
+				samR.add(new SamR(samIters[first.ind].next(),first.ind));
+			}
+			return sr;
+		}
+		
 	}
+	
+	
+	
+	
+	//chroms is string e.g 0:1:2  or 0,0,2400000:1,0,240000
+public static Iterator<SAMRecord>  getCombined(Iterator<SAMRecord>[] samReaders){
 		int len = samReaders.length;
-	//	SAMRecordIterator[] samIters = new SAMRecordIterator[len];
-		//for (int ii = 0; ii < len; ii++) {
-		//	samIters[ii] = samReaders[ii].iterator();
-		//}
-		//if(reads==null && chrom_indices_to_include==null && samReaders.length==1){
-		//	samIter = samIters[0];
-	//	}else{
-			return 		new CombinedIterator(samReaders, max_reads,reads, chromIndices);
-		//}
+		if(samReaders.length==1 && false){
+			return samReaders[0];
+		}else{
+			return 		new CombinedIterator(samReaders);
+		}
 		
 	}
 
@@ -298,5 +221,54 @@ public static String minimapIndex(File refFile, String mm2, String mem, boolean 
 			p.waitFor();
 	}
 	return indexFile.getAbsolutePath();
+}
+
+public static Iterator<SAMRecord> getCombined(Iterator<SAMRecord>[] samIters, Collection[] reads, Integer max_reads,
+		String chrToInclude) {
+	final Map<Integer, int[]> chrom_indices_to_include = new HashMap<Integer, int[]>();
+	final Set<String> reads_all = new HashSet<String>();
+	for(int i=0; i<reads.length; i++){
+		for(Iterator<String> it = reads[i].iterator(); it.hasNext();){
+			reads_all.add(it.next());
+		}
+	}
+//	Map<String, int[]> chromsToInclude = new HashMap<String, int[]>();
+	if(chrToInclude!=null && !chrToInclude.equals("all")){
+		String[] chri = chrToInclude.split(":");
+		for(int i=0; i<chri.length; i++){
+			String[] str = chri[i].split(",");
+			int[] vals ;
+			if(str.length==1) vals = new int[] {0,Integer.MAX_VALUE};
+			else{
+				vals = new int[] {Integer.parseInt(str[1]), Integer.parseInt(str[2])};
+			}
+			chrom_indices_to_include.put(Integer.parseInt(str[0]), vals);
+		}
+	}
+//	if(chrom_indices_to_include.size()==0) chrom_indices_to_include=null;
+	final Iterator<SAMRecord>  sr = getCombined(samIters);
+	return new Iterator<SAMRecord>(){
+
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return sr.hasNext();
+		}
+
+		@Override
+		public SAMRecord next() {
+			SAMRecord nxt = sr.next();
+			while(
+					!(chrom_indices_to_include.containsKey(nxt.getReferenceIndex()) ||
+							 reads_all.contains(nxt.getReadName())
+							)
+							){
+				if(!sr.hasNext()) return null;
+				nxt = sr.next();
+			}
+			return nxt;
+		}
+		
+	};
 }
 }
